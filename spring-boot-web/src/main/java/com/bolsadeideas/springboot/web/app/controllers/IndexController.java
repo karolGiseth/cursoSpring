de78886dcdf5fc2313dcc77eccc0bd207084b111
SOPORTE <SOPORTE@10.3.1.155>
2019-10-31 17:28:28 -0500

    curso spring

---
+++ spring-boot-web/src/main/java/com/bolsadeideas/springboot/web/app/controllers/IndexController.java
@@ -0,0 +1,22 @@
+package com.bolsadeideas.springboot.web.app.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+
+@Controller 
+@RequestMapping("/app")
+public class IndexController {
+
+	@GetMapping({"/index","/","/home"})
+	public String index(Model model ) {
+		model.addAttribute("titulo","hola Spring con map");
+		return "index";
+		
+	}
+	
+	
+}
